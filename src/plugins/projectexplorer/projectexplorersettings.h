6c908f45152e91906c5cb54aa3894e7ba261bb3b
Xin Tan <170690490+opails@users.noreply.github.com>
2026-08-08 00:18:40 +0800

    Update projectexplorersettings.h

--- src/plugins/projectexplorer/projectexplorersettings.h
+++ src/plugins/projectexplorer/projectexplorersettings.h
@@ -48,6 +48,7 @@ public:
     Utils::BoolAspect saveBeforeBuild{this};
     Utils::BoolAspect useJom{this};
     Utils::BoolAspect autoDetectToolchains{this};
+    Utils::BoolAspect refreshMsvcEnvOnRestore{this};
     Utils::BoolAspect promptToStopRunControl{this};
     Utils::BoolAspect automaticallyCreateRunConfigurations{this};
     Utils::TypedSelectionAspect<SyncRunConfigs> syncRunConfigurations{this};
